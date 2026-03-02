19566a0fe169acff0c44a207b91d70d994098cf8
7on-ai <team7on.ai@gmail.com>
2026-03-02 23:50:12 +0700

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { Logo } from "./logo"
 
 export const Header = () => {
   return (
-    <div className="fixed z-50 pt-8 md:pt-14 top-20 left-0 w-full">
+    <div className="fixed z-20 pt-8 md:pt-14 top-10 left-0 w-full">
       <header className="flex items-center justify-center container">
         <Link href="/">
           <Logo className="w-[711px] md:w-[711px]" />
